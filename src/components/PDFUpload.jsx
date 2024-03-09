baf5f2094707fef53dabd93c5464bce8b0712971
jaewoogwak <jaewoo010207@gmail.com>
2024-03-09 23:59:00 +0900

    fix: PDF 다운로드 버튼 컴포넌트 사용 중지
    blob 파일을 상태값으로 전달하면 PDF 파일이 정상적으로 열리지 않는 현상

--- src/components/PDFUpload.jsx
+++ src/components/PDFUpload.jsx
@@ -102,7 +102,22 @@ const PDFUpload = () => {
         </StatusWrapper>
     ) : fileState === 'done' ? (
         <PDFViewerWrapper>
-            <PDFDownload />
+            <DownloadBtn
+                onClick={() => {
+                    const downloadUrl = window.URL.createObjectURL(
+                        new Blob([pdfFile])
+                    );
+
+                    const link = document.createElement('a');
+                    link.href = downloadUrl;
+                    link.setAttribute('download', 'study-mentor.pdf');
+                    document.body.appendChild(link);
+                    link.click();
+                    link.remove();
+                }}
+            >
+                문제 저장하기
+            </DownloadBtn>
             <StatusWrapper>
                 <PDFViewer path={URL.createObjectURL(pdfFile)} scale={1.5} />
             </StatusWrapper>
@@ -146,4 +161,19 @@ const StatusWrapper = styled.div`
     flex-direction: column;
     justify-content: center;
     font-size: 24px;
+`;
+
+const DownloadBtn = styled.button`
+    width: 100%;
+    height: 100%;
+    font-size: 24px;
+    color: #ab41ff;
+    text-decoration: none;
+    cursor: pointer;
+    border: none;
+    background-color: white;
+    margin-top: 20px;
+    &:hover {
+        color: #ff6b6b;
+    }
 `;
