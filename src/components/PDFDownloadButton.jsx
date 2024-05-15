6c7cf0312dc2879dce855d741bd4b8d251bd329a
윤아현 <74540582+Yoon-Ahyeon@users.noreply.github.com>
2024-05-15 14:43:28 +0900

    fix: 문제 생성하기, 다운로드하기 버튼 색상 변경

--- src/components/PDFDownloadButton.jsx
+++ src/components/PDFDownloadButton.jsx
@@ -5,7 +5,7 @@ const colors1 = ['#6253E1', '#04BEFE'];
 const colors2 = ['#fc6076', '#ff9a44', '#ef9d43', '#e75516'];
 const colors3 = ['#40e495', '#30dd8a', '#2bb673'];
 const getHoverColors = (colors) =>
-    colors.map((color) => new TinyColor(color).lighten(5).toString());
+    colors.map((color) => new TinyColor(color).spin(30).lighten(10).saturate(20).toString());
 const getActiveColors = (colors) =>
     colors.map((color) => new TinyColor(color).darken(5).toString());
 
